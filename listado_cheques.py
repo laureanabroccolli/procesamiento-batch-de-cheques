9c4a94114eef809db860e7092a54105a0cbc0785
Daniela Bustamante <daniela.annabella05@gmail.com>
2022-07-03 14:38:53 -0300

    corrrecion del nombre

--- listado_cheques.py
+++ listado_cheques.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('Clientes.csv') as f:
+with open('clientes.csv') as f:
   reader = csv.reader(f)
   for row in reader:
     print("DNI: {8}, Tipo: {10}, Estado: {9}, FechaOrigen: {6}, FechaPago: {7}".format(row[8],row[10],row[9],row[6],row[7]))
