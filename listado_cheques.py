78c77fdad118ef84d9ae8ee70cc2056f0b211fa1
strlla <estrellaportocarrero@gmail.com>
2022-07-04 00:11:13 -0300

    Por cada fila se crea un objeto a partir de la clase Cheque

--- listado_cheques.py
+++ listado_cheques.py
@@ -1,11 +1,13 @@
 import csv
-
+from Cheque import *
 cheques = list()
 
 with open('Clientes.csv') as f:
   reader = csv.reader(f)
   for row in reader:
-    cheques.append(row)
+    nuevo_cheque = Cheque('??', row[6], row[7])
+    cheques.append(nuevo_cheque)
     print("DNI: {0}, Tipo: {1}, Estado: {2}, FechaOrigen: {3}, FechaPago: {4}".format(row[8],row[10],row[9],row[6],row[7]))
 
+print(cheques)
 # Modificar lo anterior
