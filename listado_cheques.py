18076bd27b6bdbab7701ae0c23398e72506f6b0f
strlla <estrellaportocarrero@gmail.com>
2022-08-07 19:42:28 -0300

    Correcciones sprint 4

--- listado_cheques.py
+++ listado_cheques.py
@@ -1,136 +1,82 @@
 
+from asyncio.windows_events import NULL
 import csv
 import time
-import datetime
+from datetime import datetime
+
+def obtenerDni():
+  dni = NULL
+  while not dni:
+    dni = input("Ingrese el DNI del cliente (sin puntos): \n ".upper())
+  return dni
+
+def obtenerSalida():
+  salida = ""
+  while (salida != "PANTALLA" and salida != "CSV"):
+    salida = str(input("Ingrese una salida: PANTALLA o CSV:")).upper()
+  
+  return salida
+
+def obtenerTipo():
+  tipo = NULL
+  while not tipo:
+    tipo = input("Ingrese el estado del cheque (EMITIDO o DEPOSITADO)".upper())  
+  return tipo
+
+def obtenerEstado():
+  estado = input("Ingrese el estado del cheque: Aprobado, Rechazado o Pendiente (Opcional).".upper())
+  return estado
+
+def timeStamp(fecha): 
+  newFecha = time.mktime(datetime.datetime.strptime(fecha,"%d/%m/%Y").timetuple())
+  return newFecha
+  
+def procesarSalida(cheques, dni):
+  salida = obtenerSalida()
+  if salida == "PANTALLA":
+    for i in range(len(cheques)):
+      print(f''' ---------------------------------------------------------------------------------------
+        Cheque N°{cheques[i]["NroCheque"]}
+        
+        Fecha de Emision: {cheques[i]["FechaOrigen"]}         Fecha de Pago: {cheques[i]["FechaPago"]}
+        DNI: {cheques[i]["DNI"]}           Sucursal: {cheques[i]["CodigoSucursal"]}            Codigo: {cheques[i]["CodigoBanco"]}
+        Cuenta de Origen: {cheques[i]["NumeroCuentaOrigen"]}               Cuenta de Destino: {cheques[i]["NumeroCuentaDestino"]}
+        Tipo de Cheque: {cheques[i]["Tipo"]}         Estado del Cheque: {cheques[i]["Estado"]}
+        Monto: {cheques[i]["Valor"]} $
+      ---------------------------------------------------------------------------------''')
+
+  if salida == "CSV":
+    with open(f'cheques\{dni}_{datetime.now().strftime("%H-%M-%S")}.csv', "w") as archivo:    
+      for cheque in cheques:
+        archivo.writelines(f'Fecha de emision:{cheque["FechaOrigen"]}\n' f'Fecha de pago: {cheque["FechaPago"]}\n' f'Monto cheque: {cheque["Valor"]}\n' f'Cuenta de origen: {cheque["NumeroCuentaOrigen"]}\n')
+      archivo.close()  
+    
 
 def procesarCheques():
-  file=input('INGRESE EL NOMBRE DEL ARCHIVO .CSV A PROCESAR (*Tengase en cuenta que el csv deberá estar dentro de este):\n-->  '.upper())
-  data= open(f'{file}.csv','r') 
+  file=input('INGRESE EL NOMBRE (CON EL FORMATO) DEL ARCHIVO A PROCESAR (*Tengase en cuenta que el archivo deberá estar dentro de este):\n-->  '.upper())
+  data= open(f'{file}','r') 
   csvReader=csv.DictReader(data) 
   
-  operaciones=input('INGRESE LA OPERACION A REALIZAR:\n 1) --Ver Cheques Aprobados\n 2) ---Ver cheques rechazados\n 3) ---Ver todos los cheques\n 4) ---Exportar todos mis depositos\n 5) ---Salir\n--> '.upper())
-  def timeStamp(fecha): 
-    newFecha= time.mktime(datetime.datetime.strptime(fecha,"%d/%m/%Y").timetuple())
-    return newFecha
-  def switcher(opcion):
-    
-    def opcion1():
-     datoPorConsola=input('Ingrese su dni \n--- '.upper())
-     print('Sus cheques Aprobados son:')
-     for cheque in csvReader:
-        if cheque['DNI']==datoPorConsola and cheque['Estado']=='Aprobado':
-          print(f'''-------------------------------------------------------------------------------------------------
-            Cheque N°: {cheque["NroCheque"]}                          EMISOR: {cheque['Nombre']} {cheque['Apellido']} 
-
-            Fecha de Emision: {timeStamp(cheque["fechaEmision"])}         Fecha de Pago: {timeStamp(cheque["FechaPago"])} 
-
-            DNI: {cheque["DNI"]}     BANCO: {cheque['Banco']}       Sucursal: {cheque["CodigoSucursal"]}            Codigo: {cheque["CodigoBanco"]}
-
-            Cuenta de Origen: {cheque["NumeroCuentaOrigen"]}               Cuenta de Destino: {cheque["NumeroCuentaDestino"]}
-
-            Tipo de Cheque: {cheque["Tipo"]}         Estado del Cheque: {cheque["Estado"]}
-
-            Monto: ${cheque["Valor"]} 
-            --------------------------------------------------------------------------------------------------''')         
-          operacion=input('\nINGRESE LA OPERACION A REALIZAR:\n1)--- Exportar \n2)--- Salir\n--> '.upper())
-          if operacion=='1' or operacion=='EXPORTAR':
-           nombreArchivo=input('Ingrese el nombre de su archivo CSV\n'.upper())
-           fieldnames = ['Nombre','Apellido','Cheque N°', 'EMISOR','FECHA EMISION','FECHA PAGO','DNI','BANCO','Sucursal','Codigo','Cuenta Origen','Cuenta destino','Tipo cheque','Estado','Valor']
-
-           with open(f'{nombreArchivo}.csv', 'w',) as csvfile:
-             writer=csv.writer(csvfile)
-             writer.writerow(fieldnames)
-             for cheque in csvReader:
-
-                if cheque['DNI']==datoPorConsola :
-                 try:
-                
-                  data=[cheque['Nombre'],cheque['Apellido'],cheque['NroCheque'],cheque['Banco'],cheque['NumeroCuentaDestino'],cheque['NumeroCuentaOrigen'],cheque['Valor'],cheque['fechaEmision'],cheque['FechaPago'],cheque['Fondos'],cheque['Estado'],cheque['Tipo']]
-                  writer.writerow(data)
-                  print('Archivo creado exitosamente')
-
-                 except:
-                  print('Lo sentimos ha habido un problema')
-          elif operacion=='2' or operacion=='SALIR':
-            break
-    def opcion2():
-      datoPorConsola=input('Ingrese su dni \n--- '.upper())
-      print('Estos son sus cheques Rechazados:')
-      for cheque in csvReader:
-           if cheque['DNI']==datoPorConsola and cheque['Estado']=='Rechazado':
-            print(f'''-------------------------------------------------------------------------------------------------
-            Cheque N°: {cheque["NroCheque"]}                          EMISOR: {cheque['Nombre']} {cheque['Apellido']} 
-
-            Fecha de Emision: {timeStamp["fechaEmision"]}         Fecha de Pago: {timeStamp(cheque["FechaPago"])} 
-
-            DNI: {cheque["DNI"]}     BANCO: {cheque['Banco']}       Sucursal: {cheque["CodigoSucursal"]}            Codigo: {cheque["CodigoBanco"]}
-
-            Cuenta de Origen: {cheque["NumeroCuentaOrigen"]}               Cuenta de Destino: {cheque["NumeroCuentaDestino"]}
-
-            Tipo de Cheque: {cheque["Tipo"]}         Estado del Cheque: {cheque["Estado"]}
-
-            Monto: ${cheque["Valor"]} 
-            --------------------------------------------------------------------------------------------------''')    
-    def opcion3 ():
-      datoPorConsola=input('Ingrese su dni \n--- '.upper())
-      print('Estos son todos sus cheques:')
-      for cheque in csvReader:
-           if cheque['DNI']==datoPorConsola and cheque['Estado']:
-            print(f'''-------------------------------------------------------------------------------------------------
- Cheque N°: {cheque["NroCheque"]}                          EMISOR: {cheque['Nombre']} {cheque['Apellido']} 
- 
- Fecha de Emision: {timeStamp(cheque["fechaEmision"])}         Fecha de Pago: {timeStamp(cheque["FechaPago"])} 
-
- DNI: {cheque["DNI"]}     BANCO: {cheque['Banco']}       Sucursal: {cheque["CodigoSucursal"]}            Codigo: {cheque["CodigoBanco"]}
-
- Cuenta de Origen: {cheque["NumeroCuentaOrigen"]}               Cuenta de Destino: {cheque["NumeroCuentaDestino"]}
-
- Tipo de Cheque: {cheque["Tipo"]}         Estado del Cheque: {cheque["Estado"]}
-
- Monto: ${cheque["Valor"]} 
- --------------------------------------------------------------------------------------------------''')
-    def opcion4():
-        datoPorConsola=input('Ingrese su dni \n--- '.upper())
-        nombreArchivo=input('Ingrese el nombre de su archivo CSV\n'.upper())
-        fieldnames = ['Nombre','Apellido','Cheque N°', 'EMISOR','FECHA EMISION','FECHA PAGO','DNI','BANCO','Sucursal','Codigo','Cuenta Origen','Cuenta destino','Tipo cheque','Estado','Valor']
-        with open(f'{nombreArchivo}.csv', 'w',) as csvfile:
-         writer=csv.writer(csvfile)
-         writer.writerow(fieldnames)
-         for cheque in csvReader:
-           
-            if cheque['DNI']==datoPorConsola :
-             try:
-           
-              data=[cheque['Nombre'],cheque['Apellido'],cheque['NroCheque'],cheque['Banco'],cheque['NumeroCuentaDestino'],cheque['NumeroCuentaOrigen'],cheque['Valor'],cheque['fechaEmision'],cheque['FechaPago'],cheque['Fondos'],cheque['Estado'],cheque['Tipo']]
-              writer.writerow(data)
-              print('Archivo creado exitosamente')
-              
-             except:
-              print('Lo sentimos ha habido un problema')
-      
-
-
-              
-              
-              
-    def opcion5():
-      print('Gracias por elegirnos!')
-      exit()
-
-    switcher = {
-        '1': opcion1,
-        '2': opcion2,
-        '3': opcion3,
-        '4': opcion4,
-        '5': opcion5
-    }
-  
-    func= switcher.get(opcion,lambda: "Lo sentimos la opción no es valida")
-    print(func())    
-  switcher(operaciones)
-
-  while operaciones!='5':
-    procesarCheques()
-
+  dni = obtenerDni()
+  tipo = obtenerTipo()
+  estado = obtenerEstado()
+  obj_cheque = {}
+  cheques = list()
+  for cheque in csvReader:
+      if cheque["Tipo"].upper() ==tipo.upper() and cheque['DNI']==dni and (estado.upper() == cheque["Estado"].upper() or not estado):
+        obj_cheque["DNI"] = cheque["DNI"]
+        obj_cheque["NroCheque"] = cheque["NroCheque"]
+        obj_cheque["CodigoBanco"] = cheque["CodigoBanco"]
+        obj_cheque["Tipo"] = cheque["Tipo"]
+        obj_cheque["Estado"] = cheque["Estado"]
+        obj_cheque["CodigoSucursal"] = cheque["CodigoSucursal"]
+        obj_cheque["NumeroCuentaOrigen"] = cheque["NumeroCuentaOrigen"]
+        obj_cheque["FechaOrigen"] = cheque["FechaOrigen"]
+        obj_cheque["Valor"] = cheque["Valor"] 
+        obj_cheque["NumeroCuentaDestino"] = cheque["NumeroCuentaDestino"]
+        obj_cheque["FechaPago"] = cheque["FechaPago"]
+        
+        cheques.append(obj_cheque.copy())        
+  procesarSalida(cheques, dni)
 
 procesarCheques()
